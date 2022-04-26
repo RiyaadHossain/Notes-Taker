71571076e7e109812d2c5c635d3e76564fe6f3bd
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-26 17:44:56 +0600

    Function: Search notes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header/Header";
 import InputForm from "./components/inputForm/InputForm";
 import NoteCard from "./components/noteCard/NoteCard";
 import { useEffect, useState } from "react";
 
-
-
 function App() {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-
-   fetch("notes.json")
-   .then(res=>res.json())
-   .then(data=>setNotes(data))
-
-    
+    fetch("http://localhost:5000/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
   }, []);
-  /*
-1. here there will be a function named handleSearch
-to handle search by query, and it will be passed as props to header
-
-  */
   
-
-
-
-
-
-
+// 1. Function: To search Notes 
   
-/*2. here there will be a function named handleDelete
-to delete a note, and it will be passed as props to NoteCard that will be triggered using delete button.
- */
+  const searchNote = e => {
+    e.preventDefault()
+    const searchText = e.target.searchText?.value
+    
+      fetch(`http://localhost:5000/notes?user_name=${searchText}`)
+        .then((res) => res.json())
+        .then((data) => setNotes(data));
+    
+  }
 
-
-
-
-
-
-
-
-
-
+  // 2.Function: To Delete Note 
 
   /*
 3. there will be a function named handleUpdate
@@ -51,28 +33,18 @@ to delete a note, and it will be passed as props to NoteCard that will be trigge
    later it will be passed to Update modal using props.
  */
 
-
-
-   
-
-
-
   /*
 4.  there will be a function named handlePost
 to post data to backend, and it will be passed as props to InputFrom.
  */
 
-  
-
   return (
     <div className="App">
-      <Header  />
+      <Header searchNote={searchNote} />
       <InputForm />
       <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
         {notes.map((note) => (
-          <NoteCard
-            note={note}
-          />
+          <NoteCard key={note._id} note={note} />
         ))}
       </div>
     </div>
